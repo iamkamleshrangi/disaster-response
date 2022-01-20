30cdbb0d6878fde877f633924f6963db807ae02c
Kamlesh Kumar Rangi <iamkamleshrangi@gmail.com>
2022-01-20 22:53:27 +0530

    Fixed everything in app, model, template

--- app/run.py
+++ app/run.py
@@ -8,11 +8,34 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+#from sklearn.externals import joblib
 from sqlalchemy import create_engine
+import pickle
+from joblib import dump, load
+from sklearn.base import BaseEstimator, TransformerMixin
+import nltk
+from plotly.graph_objs import Bar, Pie
 
 
 app = Flask(__name__)
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ["VB", "VBP"] or first_word == "RT":
+                return True
+        return False
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+
 
 def tokenize(text):
     tokens = word_tokenize(text)
@@ -26,71 +49,86 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
-
+engine = create_engine('sqlite:///../data/disaster_response_table.db')
+df = pd.read_sql_table('data/disaster_response_table.db', engine)
+print(df.head())
 # load model
-model = joblib.load("../models/your_model_name.pkl")
-
+#model = joblib.load("../models/your_model_name.pkl")
+model = load("classifier.joblib")
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    
-    # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
-    genre_counts = df.groupby('genre').count()['message']
+    genre_counts = df.groupby("genre").count()["message"]
+    print('Names ',genre_counts)
+    print('****'*100)
     genre_names = list(genre_counts.index)
-    
+
+    category_counter = df.drop(["id", "message", "original", "genre"], axis=1).sum()
+    category_counter.sort_values(ascending=False, inplace=True)
+    category_names = list(category_counter.index)
+
     # create visuals
-    # TODO: Below is an example - modify to create your own visuals
     graphs = [
         {
-            'data': [
-                Bar(
-                    x=genre_names,
-                    y=genre_counts
+            "data": [
+                Pie(
+                    hole=0.4,
+                    name="Genre",
+                    pull=0,
+                    marker=dict(colors=["#9AD59A", "#B4B8D0", "#F5BE51"]),
+                    textinfo="label+percent+value",
+                    hoverinfo="all",
+                    labels=genre_names,
+                    values=genre_counts,
+                    uid="95cd0b0c",
                 )
             ],
-
-            'layout': {
-                'title': 'Distribution of Message Genres',
-                'yaxis': {
-                    'title': "Count"
-                },
-                'xaxis': {
-                    'title': "Genre"
-                }
-            }
-        }
+            "layout": {
+                "title": "Distribution of Message Genres",
+                "titlefont": {"size": 18},
+                "yaxis": {"title": "Count"},
+                "xaxis": {"title": "Genre"},
+            },
+        },
+        {
+            "data": [
+                Bar(x=category_names, y=category_counter, marker=dict(color="#463659"))
+            ],
+            "layout": {
+                "title": "Distribution of Message Categories",
+                "titlefont": {"size": 18},
+                "yaxis": {"title": "Count"},
+                "xaxis": {"title": "Category", "tickangle": 35, "automargin": True},
+            },
+        },
     ]
-    
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    print(ids)
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+    print(graphJSON)
+
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
-
 
 # web page that handles user query and displays model results
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '') 
+    query = request.args.get('query', '')
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
-    # This will render the go.html Please see that file. 
+    # This will render the go.html Please see that file.
     return render_template(
         'go.html',
         query=query,
         classification_result=classification_results
     )
-
 
 def main():
     app.run(host='0.0.0.0', port=3001, debug=True)
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,13 +1,47 @@
-import sys
+import sys, re, nltk
+from sqlalchemy import create_engine
+import pandas as pd
 
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+nltk.download(['punkt','stopwords','wordnet','averaged_perceptron_tagger'])
+
+lemmatizer = WordNetLemmatizer()
 
 def load_data(database_filepath):
-    pass
-
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql_table(database_filepath, engine)
+    X = df['message']
+    y = df.drop(['id','message','original','genre'], axis=1)
+    labels = y.columns.value
+    return X, y, labels
 
 def tokenize(text):
-    pass
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
 
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 def build_model():
     pass
@@ -27,13 +61,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
